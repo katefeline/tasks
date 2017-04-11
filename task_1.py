67b3dfb9a450eb6b7bf4d752d04327b2796a47df
katefeline <katefeline@gmail.com>
2017-04-11 20:32:08 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,23 @@
+import math
+#task1
+a = 5
+b = 7
+c = 9
+print (a + b * ( c / 2 ))
+
+#task2
+print ((a**2 + b**2)%2)
+
+#task3
+print (( a + b ) / 12 * c % 4 + b)
+
+#task4
+print ((a - b * c ) / ( a + b ) % c)
+
+#task5
+d=(a-b)
+print ( d/( a + b)**3 - math.cos (c))
+
+#task6
+#( ln( 1 + c ) / -b )4+ | a |
+print ((math.log (1+c) / (- b))**4 + a)
